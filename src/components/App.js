55674885c85548792ec2c316500a941b667a895d
Tanuchouksey <ertanuchouksey@gmail.com>
2023-12-08 15:40:30 +0530

    assig done

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,3 @@
-
-/*import React from "react";
-import './../styles/App.css';
-
-const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */
-        
-        /*}
-    </div>
-  );
-};
-
-export default App.
-*/
-
 import React from "react";
 import './../styles/App.css';
 import Input from "./Input";
